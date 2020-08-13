3448253af5ccf0128abb50f4295135c635ed72ee
GuilhermeCesar029 <guicesar029@gmail.com>
2020-08-13 11:58:55 -0300

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Oficina/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+  import Oficina from './components/Oficina'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Oficina
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
---
+++ src/components/Oficina.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Clientes</h2>
+    </div>    
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
