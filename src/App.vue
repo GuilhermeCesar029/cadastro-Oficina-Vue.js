5e419c54dec3c8c61db44263078a80a9b705c35e
GuilhermeCesar029 <guicesar029@gmail.com>
2020-08-13 12:46:10 -0300

    criando formulário e armazenando em um array

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,61 @@
 <template>
   <div id="app">
-    <Oficina/>
+    <h3>Cadastro de Clientes</h3>
+    <input type="text" placeholder="nome" v-model="nomeField"><br>
+    <input type="text" placeholder="sobrenome" v-model="sobreNomeField"><br>
+    <input type="text" placeholder="email" v-model="emailField"><br>
+    <input type="number" placeholder="CPF" v-model="cpfField"><br>
+    <input type="text" placeholder="placa do carro" v-model="placaField"><br>
+    <button @click="cadastrarUsuario">Cadastrar</button>
   </div>
 </template>
 
 <script>
-  import Oficina from './components/Oficina'
+  //import Oficina from './components/Oficina'
 
 export default {
   name: 'App',
-  components: {
+  data(){
+    return {
+      nomeField: "",
+      sobreNomeField: "",
+      emailField: "",
+      cpfField: "",
+      placaField: "",
+      clientes: [
+        {
+          id:1,
+          nome: "Guilherme",
+          sobrenome: "césar",
+          email: "guilherme@gmail.com",
+          cpf: 122222,
+          placa: "ass22"
+        }
+      ]
+    }
+  },
+  /*components: {
     Oficina
+  }*/
+  methods: {
+    cadastrarUsuario: function(){
+      this.clientes.push({
+        nome: this.nomeField, 
+        sobrenome: this.sobreNomeField, 
+        email: this.emailField, 
+        cpf: this.cpfField, 
+        placa: this.placaField, 
+        id: Date.now() //data atual
+
+      })
+
+      //limpar campos
+      this.nomeField = "";
+      this.sobreNomeField = "";
+      this.emailField = "";
+      this.cpfField = "";
+      this.placaField = "";
+    }
   }
 }
 </script>
