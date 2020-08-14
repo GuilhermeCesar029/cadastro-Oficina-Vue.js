0fe464ba294b9257df1085336964a01c863f2c23
GuilhermeCesar029 <guicesar029@gmail.com>
2020-08-13 22:11:33 -0300

    adicionando o v-mask

--- src/App.vue
+++ src/App.vue
@@ -4,19 +4,12 @@
     <input type="text" placeholder="nome" v-model="nomeField"><br>
     <input type="text" placeholder="sobrenome" v-model="sobreNomeField"><br>
     <input type="text" placeholder="email" v-model="emailField"><br>
-    <input type="number" placeholder="CPF" v-model="cpfField"><br>
-    <input type="text" placeholder="placa do carro" v-model="placaField"><br>
-    <button @click="cadastrarUsuario">Cadastrar</button>
-    <hr>
-                                      <!--key para identificar um cliente do outro-->
-    <div v-for="cliente in clientes" :key="cliente.id">
-      <Oficina :cliente="cliente"/>
-    </div>
+    <input type="text" placeholder="###.###.###-##" v-model="cpfField"  v-mask="'###.###.#####'"><br>
+    <input type="text" placeholder="AAA-###" v-model="placaField" v-mask="'AAA-###'"><br>
   </div>
 </template>
 
 <script>
-  import Oficina from './components/Oficina'
 
 export default {
   name: 'App',
@@ -27,41 +20,12 @@ export default {
       emailField: "",
       cpfField: "",
       placaField: "",
-      clientes: [
-        {
-          id:1,
-          nome: "Guilherme",
-          sobrenome: "césar",
-          email: "guilherme@gmail.com",
-          cpf: 122222,
-          placa: "ass22"
-        }
-      ]
     }
   },
   components: {
-    Oficina
-  },
-  methods: {
-    cadastrarUsuario: function(){
-      this.clientes.push({
-        nome: this.nomeField, 
-        sobrenome: this.sobreNomeField, 
-        email: this.emailField, 
-        cpf: this.cpfField, 
-        placa: this.placaField, 
-        id: Date.now() //data atual
-
-      })
-
-      //limpar campos
-      this.nomeField = "";
-      this.sobreNomeField = "";
-      this.emailField = "";
-      this.cpfField = "";
-      this.placaField = "";
-    }
+  
   }
+  
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueMask from 'v-mask'
+
+Vue.use(VueMask);
 
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+
