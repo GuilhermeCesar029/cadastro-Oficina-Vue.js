4fe6554c50595358ab0592973702381d385191c6
GuilhermeCesar029 <guicesar029@gmail.com>
2020-08-18 20:27:44 -0300

    adicionando componente

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,18 @@
 <template>
   <div id="app">
     <h3>Cadastro de Clientes</h3>
-    <form action="">
-      <input type="text" placeholder="nome" v-model="nomeField" required><br>
-      <input type="text" placeholder="sobrenome" v-model="sobreNomeField" required><br>
-      <input type="email" placeholder="email" v-model="emailField" required><br>
-      <input type="text" placeholder="###.###.###-##" v-model="cpfField"  v-mask="'###.###.###-##'" required><br>
-      <input type="text" placeholder="AAA-####" v-model="placaField" v-mask="'AAA-####'" required><br>
-      <button type="submit">cadastrar</button>
-    </form>
+    <cadastro/>
   </div>
 </template>
 
 <script>
+  import cadastro from './components/cadastro'
 
-export default {
-  name: 'App',
-  data(){
-    return {
-      nomeField: "",
-      sobreNomeField: "",
-      emailField: "",
-      cpfField: "",
-      placaField: "",
+  export default {
+    name: 'App',
+    components: {
+      cadastro
     }
-  },
-  components: {
-  
   }
-  
-}
 </script>
 
-<style>
-  #app {
-    text-align: center;
-  }
-</style>
---
+++ src/components/cadastro.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <form action="">
+            <label for="">Nome: </label>
+            <input type="text" placeholder="nome" v-model="nomeField" required><br>
+
+            <label for="">sobrenome: </label>
+            <input type="text" placeholder="sobrenome" v-model="sobreNomeField" required><br>
+
+            <label for="">Email: </label>
+            <input type="email" placeholder="email" v-model="emailField" required><br>
+
+            <label for="">CPF:</label>
+            <input type="text" placeholder="###.###.###-##" v-model="cpfField"  v-mask="'###.###.###-##'" required><br>
+            
+            <label for="">Placa do veiculo</label>
+            <input type="text" placeholder="AAA-####" v-model="placaField" v-mask="'AAA-####'" required><br>
+            <button type="submit">cadastrar</button>
+        </form>
+    </div>
+</template>
+
+<script>
+
+export default {
+  data(){
+    return {
+      nomeField: "",
+      sobreNomeField: "",
+      emailField: "",
+      cpfField: "",
+      placaField: "",
+    }
+  }  
+}
+</script>
+
+<style>
+  #app {
+    text-align: center;
+  }
+</style>
